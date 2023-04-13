42fa905eeb3f5b79a16636b3babed4a9499c7b30
ShreyanshPandey73 <117717418+ShreyanshPandey73@users.noreply.github.com>
2023-04-13 19:57:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const student = {
+  name: 'John Doe'
+};
+
+// Add a 'getKeys()' method to the Object prototype
+Object.prototype.getKeys = function() {
+  // Use Object.keys() to get an array of all the keys in the object
+  return Object.keys(this);
+};
